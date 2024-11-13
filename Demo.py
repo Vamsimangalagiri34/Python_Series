9154a948eab6c8189b867503ba0e18ab85224332
vamsi34 <vamsim7989@gmail.com>
2024-11-13 19:13:28 +0530

    save 2

--- Demo.py
+++ Demo.py
@@ -1,6 +1,9 @@
-set1 = {"apple", "banana", "cherry"}
-set2 = {"google", "microsoft", "apple"}
+class Demo :
+    def __init__(self):
+        print("initiated")
+    def oneFun(self):
+        print('from onefun')
 
-set3 = set1.symmetric_difference(set2)
-
-print(set3)
+obj=Demo()
+obj.oneFun()
+Demo.oneFun(obj)
